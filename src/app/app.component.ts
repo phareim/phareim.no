5c851fda27ff0ca32e4ea45b14b6525cc54cd940
Petter Hareim <petter.hareim@bouvet.no>
2017-10-19 23:15:24 +0200

    clickable things, now shuffled.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,9 +14,26 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        const number = Math.floor(Math.random() * 100) + 1;
-        for (let i = 0; i < number; i++) {
+        const number = Math.floor(Math.random() * 70) + 30;
+        for (let i = 1; i < number; i++) {
             this.things.push(<Thing>{title: i.toString()});
         }
+        this.shuffle(this.things);
+    }
+
+    shuffle(array: any[]): any[] {
+        let currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+        return array;
     }
 }
