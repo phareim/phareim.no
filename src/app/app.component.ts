249e74eee0853a8cdf5a5605e50531c1cf14dca1
Petter Hareim <petter.hareim@bouvet.no>
2017-10-19 23:11:07 +0200

    clickable things.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Thing} from './stuff/thing';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+    title = 'phareim.no';
+    things: Thing[] = [];
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+        const number = Math.floor(Math.random() * 100) + 1;
+        for (let i = 0; i < number; i++) {
+            this.things.push(<Thing>{title: i.toString()});
+        }
+    }
 }
