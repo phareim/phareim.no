195a2301ef44aff86cc4f4d7ea3cb4cec903cbb5
Petter Hareim <petter.hareim@bouvet.no>
2017-10-24 22:46:24 +0200

    route to 1 and back.

--- src/app/projects/p1/p1.component.ts
+++ src/app/projects/p1/p1.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Color} from '../../stuff/s/color';
 
 @Component({
-  selector: 'app-p1',
-  templateUrl: './p1.component.html',
-  styleUrls: ['./p1.component.css']
+    selector: 'app-p1',
+    templateUrl: './p1.component.html',
+    styleUrls: ['./p1.component.css']
 })
 export class P1Component implements OnInit {
+    color: string;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.color = Color.getRandomColor();
+    }
 
+    c() {
+        this.color = Color.getRandomColor();
+    }
 }
