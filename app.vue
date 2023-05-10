604d51de00d4111038d48e63aaa6b71382790694
Petter Hareim <petter.hareim@hkraft.no>
2023-05-10 22:41:02 +0200

    🧹

--- app.vue
+++ app.vue
@@ -1,5 +1,28 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <header>
+      <h1>build bad art</h1>
+    </header>
   </div>
 </template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap");
+div {
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  height: 100vh;
+}
+header {
+  color: #333;
+  text-align: center;
+  font-family: "Comfortaa", cursive;
+}
+h1 {
+  font-size: 4em;
+  font-weight: 300;
+  margin: 0;
+  padding: 0;
+}
+</style>
