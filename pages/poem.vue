a034f5c071cabc7bbbc42930c5e75d6cb7a380ab
Petter Hareim <petter.hareim@hkraft.no>
2023-10-16 08:53:05 +0200

    add a very bad poem

---
+++ pages/poem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="poem">
+    <p>this is a terrible poem</p>
+  </div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap");
+div {
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  height: 95vh;
+  margin: 0;
+  padding: 0;
+  color: #333;
+  text-align: center;
+  font-family: "Comfortaa", cursive;
+}
+p {
+  font-size: 3em;
+  font-weight: 300;
+  margin: 0;
+  padding: 0;
+}
+</style>
