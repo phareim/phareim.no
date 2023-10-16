70dcb9d7e73dfbb828fa2645b427a5941aeebf78
Petter Hareim <petter.hareim@hkraft.no>
2023-10-16 09:30:33 +0200

    add NuxtPage 🧹

---
+++ pages/index.vue
@@ -0,0 +1,130 @@
+<template>
+  <div @touchstart="animateText"   :style="{ fontFamily: currentFont }">
+      <h1 :style="{
+        color: textColor,
+        transform: `scale(${textScale}) translateY(${textYOffset}vh) rotate(${textRotation}deg)`
+      }" :class="{ transition: isTransitionActive }"
+>
+{{currentQuote}}
+</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quotes: [
+        'Create Bad Art',
+        'Code Creative Chaos',
+        'Debug Dreamy Designs',
+        'Script Stylish Stories',
+        'Test Terrific Themes',
+    'Mold Magical Methods',
+    'Brew Brilliant Blueprints',
+    'Fabricate Fabulous Frameworks',
+    'Generate Gorgeous Graphics',
+    'Illustrate Impressive Interfaces',
+    'Sculpt Spectacular Software',
+    'Draft Daring Databases',
+    'Author Artistic Algorithms',
+    'Devise Distinguished Designs',
+    'Concoct Clever Codebases',
+    'Forge Funky Functions',
+    'Render Radiant Renders',
+    'Bootstrap Breathtaking Builds',
+    'Compose Captivating Code',
+    'Assemble Amazing Apps',
+    'Develop Dazzling Designs',
+    'Engineer Elegant Elements',
+    'Craft Creative Components',
+    'Produce Polished Products',
+    'Structure Stunning Systems',
+		'Articulate Bad Consepts'
+],
+			currentQuote: '', 
+			isTransitionActive: false,  // Ny flagg for å styre overgangsstaten
+			textYOffset: 0,  // Initial vertical offset
+      textRotation: 0,  // Initial rotation
+      isTextAnimated: false,
+      textColor: '#333',  // Initial color
+			currentFont: '"Comfortaa", arial ,cursive',  // Initial font
+			textScale: 1,
+    }
+  },
+	created() {
+    this.pickRandomQuote();  // Velger et tilfeldig sitat når komponenten er opprettet
+  },
+  methods: {
+    animateText() {
+      this.isTextAnimated = !this.isTextAnimated;
+      this.setRandomBgColor();  // Set a random background color
+		  this.setTextColor();
+			this.setRandomScale();
+			this.setRandomTransform(30);
+			this.pickRandomQuote();  // Velg et nytt tilfeldig sitat
+      this.toggleTransition();  // Aktiver overgangseffekten
+		},
+		pickRandomQuote() {
+      // Sørger for at det nye sitatet er forskjellig fra det nåværende
+      let newQuote;
+      do {
+        newQuote = this.quotes[Math.floor(Math.random() * this.quotes.length)];
+      } while (newQuote === this.currentQuote);
+      this.currentQuote = newQuote;
+    },
+		toggleTransition() {
+      this.isTransitionActive = true;  // Aktiver overgangseffekten
+      setTimeout(() => {
+        this.isTransitionActive = false;  // Deaktiver overgangseffekten etter 300 ms
+      }, 600);
+    },
+		setRandomTransform(maxTransformValue) {
+      // Generate a random vertical offset and rotation based on the maxTransformValue parameter
+      this.textYOffset = Math.floor(Math.random() * (maxTransformValue - (-maxTransformValue) + 1) + (-maxTransformValue));
+      this.textRotation = Math.floor(Math.random() * (maxTransformValue - (-maxTransformValue) + 1) + (-maxTransformValue));
+    }, 
+		setRandomScale() {
+      // Generate a random scale value between 0.6 and 1.2
+      this.textScale = (Math.random() * (1.15 - 0.1) + 0.1).toFixed(2);
+    },
+    randomColor() {
+      const letters = '0123456789ABCDEF';
+      let color = '#';
+      for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    },
+    setTextColor() {
+      const bgColor = document.body.style.backgroundColor.slice(4, -1).split(', ');  // Extract RGB values from bgColor
+      const [r, g, b] = bgColor.map(Number);
+      const invertedR = 255 - r;
+      const invertedG = 255 - g;
+      const invertedB = 255 - b;
+      this.textColor = `rgb(${invertedR}, ${invertedG}, ${invertedB})`;  // Set text color to inverted background color
+    },
+    setRandomBgColor() {
+      const newColor = this.randomColor();
+      document.body.style.backgroundColor = newColor;
+      document.body.style.transition = 'background-color 0.06s ease-in-out';  // Optional: Add transition
+
+      const metaThemeColor = document.querySelector('meta[name=theme-color]');
+      if (metaThemeColor) {
+        metaThemeColor.setAttribute('content', newColor);
+      } else {
+        const newMetaThemeColor = document.createElement('meta');
+        newMetaThemeColor.name = 'theme-color';
+        newMetaThemeColor.content = newColor;
+        document.head.appendChild(newMetaThemeColor);
+      }
+    }
+  }
+}
+</script>
+
+<style>
+div{
+	  text-align: center;
+}
+</style>
--- pages/poem.vue
+++ pages/poem.vue
@@ -1,22 +1,10 @@
 <template>
   <div class="poem">
-    <p>this is a terrible poem</p>
+    <p>this is<br>a terrible<br> poem</p>
   </div>
 </template>
 
 <style>
-@import url("https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap");
-div {
-  display: grid;
-  justify-content: center;
-  align-content: center;
-  height: 95vh;
-  margin: 0;
-  padding: 0;
-  color: #333;
-  text-align: center;
-  font-family: "Comfortaa", cursive;
-}
 p {
   font-size: 3em;
   font-weight: 300;
