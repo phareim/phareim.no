2d2c7768a2c37447081175b7bfeb49b3156fe427
Petter Hareim <petter.hareim@hkraft.no>
2023-10-18 10:13:21 +0200

    bro, don't code on your phone..

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="about">
-<p>this is a pet project, written with love, spare time and a bit of AI. 
-</div>
+	<div class="about">
+		<p>this is a pet project, written with love, spare time and a bit of AI. </p>
+	</div>
 </template>
