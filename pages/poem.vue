b7d87f55ad74540ba702e18f3c37648a11fb5ae1
Petter Hareim <phareim@gmail.com>
2023-10-23 17:27:49 +0200

    Random poem.vue

--- pages/poem.vue
+++ pages/poem.vue
@@ -1,9 +1,34 @@
 <template>
   <div class="poem">
-    <h1>this is<br>a terrible<br> poem</h1>
+    <h1 v-html="randomPoem"></h1>
   </div>
 </template>
 
-<style>
+<script>
+export default {
+  data() {
+    return {
+      poems: [
+        'this is<br>a terrible<br>poem',
+        'roses are red<br>violets are blue<br>sugar is sweet<br>and so are you',
+        'twinkle twinkle<br>little star<br>how I wonder<br>what you are',
+        // ... flere dikt ...
+      ],
+      randomPoem: '',
+    };
+  },
+  mounted() {
+    this.randomPoem = this.getRandomPoem();
+  },
+  methods: {
+    getRandomPoem() {
+      const randomIndex = Math.floor(Math.random() * this.poems.length);
+      return this.poems[randomIndex];
+    },
+  },
+};
+</script>
 
+<style>
+  /* ... dine stiler ... */
 </style>
