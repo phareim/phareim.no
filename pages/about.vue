8b05195eb1c4463468b8a7de32ae840c6ea8966b
Petter Hareim <petter.hareim@hkraft.no>
2023-12-05 22:59:57 +0100

    got scroll?

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,27 @@
 <template>
-	<div class="about">
-		<p>this is a pet project, written with love, spare time and a bit of AI. </p>
-		<p>peace.</p>
-	</div>
+    <div class="about">
+        <p class="scrolling-text">this is a pet project, written with love, spare time and a bit of AI. </p>
+        <p class="scrolling-text">peace.</p>
+    </div>
 </template>
+
+<style scoped>
+.about {
+  overflow: hidden;
+}
+
+.scrolling-text {
+  display: inline-block;
+  position: relative;
+  animation: scroll 5s linear infinite;
+}
+
+@keyframes scroll {
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+}
+</style>
