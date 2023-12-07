d41de372dd55c2e853ec4fe2009483a3e8c1dcb4
Petter Hareim <phareim@gmail.com>
2023-12-07 16:53:17 +0100

    Update scroll

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="about">
         <p class="scrolling-text">this is a pet project, written with love, spare time and a bit of AI. </p>
-        <p class="scrolling-text">peace.</p>
+        <p>peace.</p>
     </div>
 </template>
 
