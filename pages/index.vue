ae8e0f40e171094d26f767634a0eb87384c89590
Petter Hareim <petter.hareim@hkraft.no>
2024-02-04 00:15:47 +0100

    Random speed

--- pages/index.vue
+++ pages/index.vue
@@ -106,7 +106,8 @@ export default {
       const x = event.clientX - rect.left;
       const y = event.clientY - rect.top;
       const color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
-      this.boxes.push({ x, y, vx: 2, vy: 2, size: (Math.random() * 20)+5, color });
+			const velocity = Math.random() * 4;
+      this.boxes.push({ x, y, vx: velocity , vy: velocity , size: (Math.random() * 20)+5, color });
     },
   },
 };
