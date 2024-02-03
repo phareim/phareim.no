6719293302e6d4921e6d64c8b3f4c7140f8aef5c
Petter Hareim <petter.hareim@hkraft.no>
2024-02-03 21:04:59 +0100

    npm the things

---
+++ pages/drafts/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="art" :style="transformStyle">
+    drafts
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      beta: 60,  // Pitch: vipping frem og tilbake
+      gamma: 40,  // Roll: vipping fra side til side
+    };
+  },
+  computed: {
+    transformStyle() {
+      return {
+        transform: `rotate(${this.gamma}deg) translateX(${this.gamma * 2}px) translateY(${this.beta * 2}px)`,
+      };
+    },
+  },
+  mounted() {
+    if (window.DeviceOrientationEvent) {
+      window.addEventListener('deviceorientation', this.handleOrientation);
+    }
+  },
+  beforeDestroy() {
+    if (window.DeviceOrientationEvent) {
+      window.removeEventListener('deviceorientation', this.handleOrientation);
+    }
+  },
+  methods: {
+    handleOrientation(event) {
+      this.beta = event.beta;
+      this.gamma = event.gamma;
+    },
+  },
+};
+</script>
+
+<style>
+.art {
+  font-size: 7em;
+  /* transition: transform 0.1s ease-in-out;   */
+}
+</style>
