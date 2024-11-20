9c1f0116823fa67b9a8cfb30793a2ef18744fe85
Petter Hareim <petter.hareim@miles.no>
2024-11-20 21:25:21 +0100

    goodbye twitter. hello 🦋

--- pages/index.vue
+++ pages/index.vue
@@ -33,12 +33,10 @@
             d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z">
           </path>
         </svg></a>
-        <a href="https://x.com/phareim" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50"
-          viewBox="0 0 50 50" class="twitter">
-          <path  
-          d="M 50.0625 10.4375 C 48.214844 11.257813 46.234375 11.808594 44.152344 12.058594 C 46.277344 10.785156 47.910156 8.769531 48.675781 6.371094 C 46.691406 7.546875 44.484375 8.402344 42.144531 8.863281 C 40.269531 6.863281 37.597656 5.617188 34.640625 5.617188 C 28.960938 5.617188 24.355469 10.21875 24.355469 15.898438 C 24.355469 16.703125 24.449219 17.488281 24.625 18.242188 C 16.078125 17.8125 8.503906 13.71875 3.429688 7.496094 C 2.542969 9.019531 2.039063 10.785156 2.039063 12.667969 C 2.039063 16.234375 3.851563 19.382813 6.613281 21.230469 C 4.925781 21.175781 3.339844 20.710938 1.953125 19.941406 C 1.953125 19.984375 1.953125 20.027344 1.953125 20.070313 C 1.953125 25.054688 5.5 29.207031 10.199219 30.15625 C 9.339844 30.390625 8.429688 30.515625 7.492188 30.515625 C 6.828125 30.515625 6.183594 30.453125 5.554688 30.328125 C 6.867188 34.410156 10.664063 37.390625 15.160156 37.472656 C 11.644531 40.230469 7.210938 41.871094 2.390625 41.871094 C 1.558594 41.871094 0.742188 41.824219 -0.0585938 41.726563 C 4.488281 44.648438 9.894531 46.347656 15.703125 46.347656 C 34.617188 46.347656 44.960938 30.679688 44.960938 17.09375 C 44.960938 16.648438 44.949219 16.199219 44.933594 15.761719 C 46.941406 14.3125 48.683594 12.5 50.0625 10.4375 Z">
-        </path>
-      </svg></a>
+        <a href="https://bsky.app/profile/phareim.no" target="_blank">
+          <svg fill="none" viewBox="0 0 64 57" width="28" style="width: 50; height: 50" class="bluesky">
+            <path d="M13.873 3.805C21.21 9.332 29.103 20.537 32 26.55v15.882c0-.338-.13.044-.41.867-1.512 4.456-7.418 21.847-20.923 7.944-7.111-7.32-3.819-14.64 9.125-16.85-7.405 1.264-15.73-.825-18.014-9.015C1.12 23.022 0 8.51 0 6.55 0-3.268 8.579-.182 13.873 3.805ZM50.127 3.805C42.79 9.332 34.897 20.537 32 26.55v15.882c0-.338.13.044.41.867 1.512 4.456 7.418 21.847 20.923 7.944 7.111-7.32 3.819-14.64-9.125-16.85 7.405 1.264 15.73-.825 18.014-9.015C62.88 23.022 64 8.51 64 6.55c0-9.818-8.578-6.732-13.873-2.745Z"></path></svg>
+          </a>
       <a href="https://github.com/phareim" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50"
         viewBox="0 0 50 50" class="github">
         <path 
@@ -65,6 +63,7 @@
 </template>
 
 <script>
+
 export default {
   name: 'Home',
   data() {
@@ -126,8 +125,8 @@ export default {
         }
       });
       //slow down mouse velocity
-      this.mousePosition.v.x = (this.mousePosition.v.x * 0.8);
-      this.mousePosition.v.y = (this.mousePosition.v.y * 0.8);
+      this.mousePosition.v.x = (this.mousePosition.v.x * 0.9);
+      this.mousePosition.v.y = (this.mousePosition.v.y * 0.9);
     },
     getNewShadow(strength, color = 'rgba(0, 0, 0, 0.5') {
       const shadow = {
@@ -199,6 +198,14 @@ export default {
       
     },
     checkCollisions(currentBox) {
+      // find collision with the profile-pic at the center of the canvas
+      /*const center = { x: this.$refs?.canvas?.width / 2, y: (this.$refs?.canvas?.height / 2) + 20, size: 200, shadow: { strength: 0 }, vx: 20, vy: 20 , profile: true };
+      if (this.isColliding(currentBox, center)) {
+        this.resolveCollision(currentBox, center);
+        console.log('collided with profile-pic');
+        return;
+      }*/
+
       this.boxes.forEach(box => {
         if (currentBox !== box && this.isColliding(currentBox, box)) {
           this.resolveCollision(currentBox, box);
@@ -554,8 +561,8 @@ h1 p {
   fill: #0a66c2;
 }
 
-.social-links svg.twitter:hover {
-  fill: #008AD8;
+.social-links svg.bluesky:hover {
+  fill: #0385ff;
 }
 
 @media (prefers-color-scheme: dark) {
