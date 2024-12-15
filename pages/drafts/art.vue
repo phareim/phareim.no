a0f6216f9675429eeabedc521df898dfc3d8b07e
Petter Hareim <petter.hareim@miles.no>
2024-12-15 11:07:00 +0100

    fix error in art.vue

--- pages/drafts/art.vue
+++ pages/drafts/art.vue
@@ -5,22 +5,33 @@
 </template>
 
 <script>
+const createAnimatedValue = (initialValue) => ({
+  value: initialValue,
+  grow: true
+});
+
 export default {
   data() {
     return {
-      alpha: {value: 10, grow: true},
-      beta:  {value: 20, grow: true},
-      gamma: {value: 30, grow: true},
-      zeta:  {value: 40, grow: true},
-      omega: {value: 5, grow: true},
-      zebra: {value: 6, grow: true},
-      red: {value: 100, grow: true},
-      green: {value: 150, grow: true},
-      blue: {value: 200, grow: true},
+      alpha: createAnimatedValue(10),
+      beta: createAnimatedValue(20),
+      gamma: createAnimatedValue(30),
+      zeta: createAnimatedValue(40),
+      omega: createAnimatedValue(5),
+      zebra: createAnimatedValue(6),
+      red: createAnimatedValue(100),
+      green: createAnimatedValue(150),
+      blue: createAnimatedValue(200),
+      interval: null
     };
   },
   computed: {
     transformStyle() {
+      if (!this.alpha?.value || !this.beta?.value || !this.gamma?.value || 
+          !this.zeta?.value || !this.omega?.value || !this.zebra?.value) {
+        return {};
+      }
+
       return {
         transform: `
           rotate(${this.alpha.value}deg) 
@@ -31,6 +42,10 @@ export default {
       };
     },
     colorStyle() {
+      if (!this.red?.value || !this.green?.value || !this.blue?.value) {
+        return {};
+      }
+
       return {
         color: `rgb(${this.red.value}, ${this.green.value}, ${this.blue.value})`,
       };
@@ -43,32 +58,46 @@ export default {
     if (window.DeviceOrientationEvent) {
       window.addEventListener('deviceorientation', this.handleOrientation);
     }
-    this.interval = setInterval(() => {
-      this.updateValues(this.alpha);
-      this.updateValues(this.beta);
-      this.updateValues(this.gamma);
-      this.updateValues(this.zeta);
-      this.updateValues(this.omega);
-      this.updateValues(this.zebra);
-      this.updateValues(this.red);
-      this.updateValues(this.green);
-      this.updateValues(this.blue);
-    }, 50); 
+    this.startAnimation();
   },
   beforeDestroy() {
-    if (window.DeviceOrientationEvent) {
-      window.removeEventListener('deviceorientation', this.handleOrientation);
-    }
-    clearInterval(this.interval);
+    this.cleanup();
+  },
+  beforeUnmount() {
+    this.cleanup();
   },
   methods: {
+    startAnimation() {
+      this.interval = setInterval(() => {
+        this.updateValues(this.alpha);
+        this.updateValues(this.beta);
+        this.updateValues(this.gamma);
+        this.updateValues(this.zeta);
+        this.updateValues(this.omega);
+        this.updateValues(this.zebra);
+        this.updateValues(this.red);
+        this.updateValues(this.green);
+        this.updateValues(this.blue);
+      }, 50);
+    },
+    cleanup() {
+      if (this.interval) {
+        clearInterval(this.interval);
+        this.interval = null;
+      }
+      if (window.DeviceOrientationEvent) {
+        window.removeEventListener('deviceorientation', this.handleOrientation);
+      }
+    },
     updateValues(prop) {
+      if (!prop) return;
+      
       prop.value += (prop.grow ? 1 : -1);
       prop.grow = prop.value > 255 || prop.value < 1 ? !prop.grow : prop.grow;
     },
     handleOrientation(event) {
-      this.beta = event.beta;
-      this.gamma = event.gamma;
+      if (event?.beta) this.beta = event.beta;
+      if (event?.gamma) this.gamma = event.gamma;
     },
   },
 };
@@ -76,7 +105,6 @@ export default {
 
 <style>
 .art {
-  /** covers the entire screen */
   position: fixed;
   top: 0;
   left: 0;
@@ -85,7 +113,6 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-
   font-size: 7em;
   transition: transform 0.1s ease-in-out, color 0.1s ease-in-out; 
 }
