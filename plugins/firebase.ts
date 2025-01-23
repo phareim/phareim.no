c0eff05d32468defd7866442f4a8606358480af9
Petter Hareim <petter.hareim@miles.no>
2025-01-23 15:06:40 +0100

    Think this works?

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -4,18 +4,7 @@ import { getAuth } from 'firebase/auth'
 
 export default defineNuxtPlugin(nuxtApp => {
     const config = useRuntimeConfig()
-
-    const firebaseConfig = {
-        apiKey: config.public.firebaseApiKey,
-        authDomain: config.public.firebaseAuthDomain,
-        projectId: config.public.firebaseProjectId,
-        storageBucket: config.public.firebaseStorageBucket,
-        messagingSenderId: config.public.firebaseMessagingSenderId,
-        appId: config.public.firebaseAppId,
-        measurementId: config.public.firebaseMeasurementId
-    }
-
-    const app = initializeApp(firebaseConfig)
+    const app = initializeApp(config.public.firebaseConfig)
     const auth = getAuth(app)
     const firestore = getFirestore(app)
 
