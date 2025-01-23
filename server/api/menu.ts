6333f213ae45f6581ef0460f3c4787c0f3bd0f67
Petter Hareim <petter.hareim@miles.no>
2025-01-23 13:27:27 +0100

    only show relevant stuff in the menu.

--- server/api/menu.ts
+++ server/api/menu.ts
@@ -3,12 +3,7 @@ import { defineEventHandler, getQuery } from 'h3'
 // Statisk liste over tilgjengelige sider
 const menuItems = [
   { path: '/drafts/about', title: 'About' },
-  { path: '/drafts/art', title: 'Art' },
-  { path: '/drafts/bounce', title: 'Bounce' },
-  { path: '/drafts/poem', title: 'Poem' },
-  { path: '/drafts/quote', title: 'Quote' },
   { path: '/drafts/rpg', title: 'RPG' },
-  { path: '/drafts/spin', title: 'Spin' },
 ]
 
 export default defineEventHandler(async (event) => {
