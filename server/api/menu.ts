66af0cb7e92c10c766f72bb371206a86c2a3c368
Petter Hareim <phareim@gmail.com>
2026-04-19 20:42:44 +0000

    feat: add blue.phareim.no to menu as external link

--- server/api/menu.ts
+++ server/api/menu.ts
@@ -22,7 +22,8 @@ const menuItems = [
   { path: '/morse', title: 'Morse', icon: '·−' },
   { path: 'https://dot.phareim.no', title: 'Red dot game', icon: '🔴', external: true },
  // { path: 'https://librarian.phareim.no', title: 'The Librarian', icon: '📚', external: true },
-  { path: 'https://reader.phareim.no', title: 'RSS Reader', icon: '📰', external: true }
+  { path: 'https://reader.phareim.no', title: 'RSS Reader', icon: '📰', external: true },
+  { path: 'https://blue.phareim.no', title: 'Blue', icon: '🔵', external: true }
 ]
 
 export default defineEventHandler(async (event: any) => {
